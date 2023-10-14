061e163b382849adf1c5401aa77d0dc6e8e21e72
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2023-10-14 11:28:28 +0300

    String Introduction

---
+++ src/Strings/StringIntroduction.java
@@ -0,0 +1,26 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class StringIntroduction {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        sc.close();
+
+        System.out.println(A.length() + B.length());
+
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        A = A.substring(0, 1).toUpperCase() + A.substring(1);
+        B = B.substring(0, 1).toUpperCase() + B.substring(1);
+        System.out.printf("%s %s", A, B);
+
+    }
+}
