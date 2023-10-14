6ebefc274112b7ebcef3a5550079697545d6b305
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2023-10-14 12:07:10 +0300

    Static Init Block

---
+++ src/Introduction/StaticInitBlock.java
@@ -0,0 +1,22 @@
+package Introduction;
+
+import java.util.Scanner;
+
+public class StaticInitBlock {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int B = scanner.nextInt();
+        int H = scanner.nextInt();
+        scanner.close();
+
+        try {
+            if (B <= 0 || H <= 0) {
+                throw new Exception();
+            } else {
+                System.out.println(H * B);
+            }
+        } catch (Exception e) {
+            System.out.println(e + ": Breadth and height must be positive");
+        }
+    }
+}
