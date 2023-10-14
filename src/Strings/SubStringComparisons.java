b2432d5fb77795f65f98f8855dcb3d4eb2793fad
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2023-10-14 11:31:00 +0300

    SubString Comparisons

---
+++ src/Strings/SubStringComparisons.java
@@ -0,0 +1,33 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class SubStringComparisons {
+    public static String getSmallestAndLargest(String s, int k) {
+        int maxLength = s.length() - k;
+        String smallest =s.substring(0, k);
+        String largest =s.substring(0, k);
+
+        for (int i = 0; i <= maxLength; i++) {
+            String sub = s.substring(i, i + k);
+
+            if (smallest.compareTo(sub) > 0) {
+                smallest = sub;
+            }
+            if (largest.compareTo(sub) < 0) {
+                largest = sub;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
