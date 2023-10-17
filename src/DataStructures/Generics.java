6a2f03935d0edcf6576a7a140a3f1899392dd86c
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2023-10-17 10:40:21 +0300

    Generics

---
+++ src/DataStructures/Generics.java
@@ -0,0 +1,16 @@
+package DataStructures;
+
+public class Generics {
+    static <E> void printArray(E[] array) {
+        for (E x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+        printArray(intArray);
+        printArray(stringArray);
+    }
+}
